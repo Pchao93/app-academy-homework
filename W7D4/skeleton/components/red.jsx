1a7e6d611c31a559f08b3e2c2f4f222aeec27883
Patrick <patrick.chao.14@gmail.com>
2018-01-25 00:42:35 -0800

    complete react router assignment

---
+++ W7D4/skeleton/components/red.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import Orange from './orange';
+import Yellow from './yellow';
+
+class Red extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        {/* Links here */}
+        <NavLink exact to='/red' >Red only</NavLink>
+        <NavLink to='/red/orange' >Orange</NavLink>
+        <NavLink to='/red/yellow' >Yellow</NavLink>
+
+        {/* Routes here */}
+        <Route path="/red/yellow" component={Yellow} />
+        <Route path="/red//orange" component={Orange} />
+      </div>
+
+    );
+  }
+}
+
+export default Red;
